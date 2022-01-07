9770efad0e2de4dd25b381a962c26028fa49dd29
kalyani kene <kalyanikene21@gmail.com>
2022-01-07 18:17:00 +0530

    Chat Bot

---
+++ script.js
@@ -0,0 +1,16 @@
+function talk(){
+    let know = {
+      "Hi" : "Hello, Developers Community Here.",
+      "How are you" : "Good :)",
+      "How to learn html" : "Please visit our page w3school.com",
+      "ok" : "Thank You So Much <3",
+      "Bye" : "Okay! Will meet soon. TC:).."
+    };
+    let user = document.getElementById('userBox').value;
+      document.getElementById('chatLog').innerHTML =user + "<br>";
+    if (user in know) {
+      document.getElementById('chatLog').innerHTML = know[user] + "<br>";
+    }else{
+      document.getElementById('chatLog').innerHTML = "For more details please visit our page w3school.com <br>";
+    }
+  }
